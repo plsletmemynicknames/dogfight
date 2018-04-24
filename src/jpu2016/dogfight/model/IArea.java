2d47e52bf6350bb3472367b5fa582fc1c5220854
Stephen <Stephen@Stephen-ROG>
2018-04-24 10:27:10 +0200

    IArea

---
+++ src/jpu2016/dogfight/model/IArea.java
@@ -0,0 +1,12 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Dimension;
+import java.awt.Image;
+
+public interface IArea {
+	
+	public  Dimension getDimension();
+	
+	public Image getImage();
+	
+}

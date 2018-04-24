5a284006a7d32b9abd2a90d4b2c387fa0cb59537
Stephen <Stephen@Stephen-ROG>
2018-04-24 10:57:57 +0200

    Phase 2 Model Dimension

---
+++ src/jpu2016/dogfight/model/Dimension.java
@@ -0,0 +1,29 @@
+package jpu2016.dogfight.model;
+
+public class Dimension {
+	
+//-------------------------------Attributs----------------------------
+		private int width;
+		private int height;
+		
+//-------------------------------Constructor----------------------------	
+		public Dimension(int width, int height) {
+			this.width = width;
+			this.height = height;}
+		
+		public Dimension(Dimension dimension) {}
+		
+//-------------------------------Méthodes----------------------------
+		public int getWidth() {
+			return this.getWidth();}
+		
+		public void setWidth(int width) {
+			this.width = width;}
+		
+		public int getHeight() {
+			return this.height;}
+		
+		public void setHeight(int height) {
+			this.height = height;}
+		
+}

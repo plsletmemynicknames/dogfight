4f8e77b5f49f2e9236055c0e7a41e64aa154e370
RazeN <RazeN@CHEVRE-DU-BAGLADESH>
2018-04-24 11:51:39 +0200

    unepetitephraseenmodegnignignignigni

---
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -0,0 +1,15 @@
+package jpu2016.dogfight.view;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.controller.UserOrder;
+import java.awt.event.KeyEvent;
+
+public class EventPerformer {
+	
+	public EventPerformer(IOrderPerformer orderPerformer) {}
+	
+	public void eventPerform(KeyEvent keyCode) {}
+	
+	private UserOrder keyCodeToUserOrder(int keyCode) {}
+
+}

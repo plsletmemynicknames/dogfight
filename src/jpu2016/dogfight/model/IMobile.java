ea2c57d0b33ed92c80c974f77eff5cbfdb535dc0
Stephen <Stephen@Stephen-ROG>
2018-04-24 10:46:17 +0200

    Phase 2 model

--- src/jpu2016/dogfight/model/IMobile.java
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -1,9 +1,28 @@
 package jpu2016.dogfight.model;
 
+import java.awt.Image;
+import java.awt.Point;
+
 public interface IMobile {
 	
 	public Direction getDirection();
 	
 	public void setDirection(Direction direction);
+	
+	public Point getPosition();
+	
+	public Dimension getDimension();
+	
+	public int getWidth();
+	
+	public int getHeight();
+	
+	public int getSpeed();
+	
+	public Image getImage();
+	
+	public void move();
+	
+	
 	
 }

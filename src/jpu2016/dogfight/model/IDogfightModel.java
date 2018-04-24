9761bed43a202cc9f0ffcb095e1b42f9f1b62fd7
Stephen <Stephen@Stephen-ROG>
2018-04-24 10:34:48 +0200

    IDogfightModel.java

---
+++ src/jpu2016/dogfight/model/IDogfightModel.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.model;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+public interface IDogfightModel {
+	
+	public IArea getArea();
+	
+	public void buildArea(Dimension dimension);
+	
+	public void  addMobile(Imobile Mobile);
+	
+	public void removeMobile(Imobile Mobile);
+	
+	public ArrayList<IMobile> getMobiles();
+	
+	public IMobile getMobileByPlayer(int player);
+	
+	public void setMobilesHavesMoved();
+	
+}
